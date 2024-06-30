df222b42099b96958649e3d7ae92007f30848c38
gabriel.oliveira <gabriel.oliveira@cubos.io>
2024-06-30 14:08:25 -0300

    feat: initial setup

---
+++ android/app/src/main/kotlin/com/gabril/oliveira/timer_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gabril.oliveira.timer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/values/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFFd40036);
+  static const Color secondary = Color(0xFF0e0d1d);
+  static const Color colorText = Color(0xFF262533);
+  static const Color white = Color(0xFFffffff);
+
+  static const Color swatch_01 = Color(0xFF040306);
+  static const Color swatch_02 = Color(0xFFb3000a);
+  static const Color swatch_03 = Color(0xFF54535a);
+  static const Color swatch_04 = Color(0xFF550007);
+  static const Color swatch_05 = Color(0xFFdeddde);
+  static const Color swatch_06 = Color(0xFF7d7c84);
+  static const Color swatch_07 = Color(0xFFd83a57);
+}
